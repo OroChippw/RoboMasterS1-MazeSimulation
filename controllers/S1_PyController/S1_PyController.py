14cd97eda8bd089a0fd1a4596f6a225c8acd610b
OroChippw <OroChippw@163.com>
2023-11-24 18:03:31 +0800

    [WIP]feat:func Macanum horizontal movement

--- controllers/S1_PyController/S1_PyController.py
+++ controllers/S1_PyController/S1_PyController.py
@@ -3,6 +3,7 @@
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot , Keyboard
+from math import sin , cos
 
 MAX_SPEED = 15
 SENSOR_THRESHOLD = 70 # sensitivity of sensors surrounding robot for obstacle detection
@@ -36,12 +37,28 @@ def stop(wheelsNames , wheels):
 Vx = Vy= 0 # linear velocities (Vx and Vy) , Positive are ahead and right
 w = 0 # angular velocity (w) , Positive are counterclockwise
 # Calculating individual wheel velocities based on linear and angular velocities.
-def Mecanum_Calculator(Vx , Vy , w , idx):
+def Mecanum_Calculator(Vx , Vy , w , idx , max_velocity=None):
     motorVelocity = [0 , 0 , 0 , 0]
-    motorVelocity[0] = Vy - Vx + w * (Vx + Vy)
-    motorVelocity[1] = Vy + Vx - w * (Vx + Vy)
-    motorVelocity[2] = Vy - Vx - w * (Vx + Vy)
-    motorVelocity[3] = Vy + Vx + w * (Vx + Vy)
+    R = 1 # Mecanum radius
+    Vx_robot = Vx * cos(w) - Vy * sin(w)
+    Vy_robot = Vx * sin(w) + Vy * cos(w)
+    # motorVelocity[0] = Vy - Vx + w * (R)
+    # motorVelocity[1] = Vy + Vx - w * (R)
+    # motorVelocity[2] = Vy - Vx - w * (R)
+    # motorVelocity[3] = Vy + Vx + w * (R)
+    if idx == 0:
+        motorVelocity[0] = Vy_robot - Vx_robot + w * (R)
+    elif idx == 1:
+        motorVelocity[1] = Vy_robot + Vx_robot - w * (R)
+    elif idx == 2:
+        motorVelocity[2] = Vy_robot - Vx_robot - w * (R)
+    elif idx == 3:
+        motorVelocity[3] = Vy_robot + Vx_robot + w * (R)
+    # print(Vx_robot , Vy_robot , motorVelocity)
+    if max_velocity is not None:    
+        for i in range(len(motorVelocity)):
+            if abs(motorVelocity[i]) > max_velocity:
+                motorVelocity[i] = max_velocity if motorVelocity[i] > 0 else -max_velocity
     
     return motorVelocity[idx]
 
@@ -71,6 +88,7 @@ def main():
     # Gets the robot's keyboard device and robot's camera device
     keyboard = robot.getKeyboard()
     keyboard.enable(1)
+    
     maincamera = robot.getDevice('camera')
     maincamera.enable(timestep)
     width = maincamera.getWidth()
@@ -96,15 +114,23 @@ def main():
         [0-fr-front right] [1-fl-front left] [2-bl-back left] [3-br-back left] 
     '''
     wheels = []
-    wheelsSpeeds = []
-    wheelsNames = ['wheel_fl_motor', 'wheel_fr_motor', 'wheel_br_motor', 'wheel_bl_motor']
-    for idx in range(len(wheelsNames)):
+    wheels_sensors = []
+    wheelsMaxSpeeds = []
+    wheelsMotorNames = ['wheel_fr_motor', 'wheel_fl_motor', 'wheel_bl_motor', 'wheel_br_motor']
+    wheels_SensorNames = ['wheel_fr_sensor', 'wheel_fl_sensor', 'wheel_bl_sensor', 'wheel_br_sensor']
+    
+    for idx in range(len(wheelsMotorNames)):
         # wheels.append(robot.getMotor(wheelsNames[idx]))
-        wheels.append(robot.getDevice(wheelsNames[idx]))
+        wheels.append(robot.getDevice(wheelsMotorNames[idx]))
+        wheels_sensors.append(robot.getDevice(wheels_SensorNames[idx]))
+        
+        wheelsMaxSpeeds.append(wheels[idx].getMaxVelocity())
         wheels[idx].setPosition(float('inf'))
         wheels[idx].setVelocity(0.0)
+    print(f"wheelsMaxSpeeds : {wheelsMaxSpeeds}")
     
-
+    changed = False
+    
     # <------ Main loop ------>
     # perform simulation steps until Webots is stopping the controller
     while robot.step(timestep) != -1:
@@ -121,8 +147,7 @@ def main():
 
         avoidObstacleCounter = 0    
         index = -1
-        changed = False
-        
+    
         key = keyboard.getKey()
         if (key == ord('W')):
             Vy = 5
@@ -132,7 +157,7 @@ def main():
                 changed = True
             else :
                 changed = False
-        if (key == ord('S')):
+        elif (key == ord('S')):
             Vy = -5
             if(index != 1) : 
                 print("[INFO] Back")
@@ -140,38 +165,57 @@ def main():
                 changed = True
             else :
                 changed = False
-        if (key == ord('D')):
-            Vx = 10
+        elif (key == ord('D')):
+            Vx = 5
+            Vy = 0.0
+            w = 0
             if(index != 2) : 
                 print("[INFO] Pan Right")
                 index = 2
                 changed = True
             else :
                 changed = False
-        if (key == ord('A')):
-            Vx = -10
+        elif (key == ord('A')):
+            Vx = -5
+            Vy = 0.0
+            w = 0
             if(index != 3) : 
                 print("[INFO] Pan Left")
                 index = 3
                 changed = True
             else :
                 changed = False
-        if (key==Keyboard.UP):
+                
+        elif (key == ord('Q')):
+            w = 1.0
+            if (index != 4):
+                index = 4
+                print("[INFO] Rotate counterclockwise")
+                changed = True
+            else :
+                changed = False
+        elif (key == ord('E')):
+            w = -1.0
+            if (index != 5):
+                index = 5
+                print("[INFO] Rotate clockwise")
+                changed = True
+            else :
+                changed = False
+        elif (key==Keyboard.UP):
             pitch_motor.setVelocity(-1.0)
             print('[INFO] Pitch axis raised')
-        if (key==Keyboard.DOWN):
+        elif (key==Keyboard.DOWN):
             pitch_motor.setVelocity(1.0)
             print('[INFO] Pitch axis down')  
-        if (key==Keyboard.LEFT):
+        elif (key==Keyboard.LEFT):
             yaw_motor.setVelocity(1.0)
             print('[INFO] YAW axis rotates clockwise')  
-        if (key==Keyboard.RIGHT):
+        elif (key==Keyboard.RIGHT):
             yaw_motor.setVelocity(-1.0)
             print('[INFO] YAW axis rotates counterclockwise') 
-        if (key==-1):
-            Vx=0
-            Vy=0
-            w=0
+        elif (key==-1):
+            Vy, Vx, w = 0, 0, 0
             pitch_motor.setVelocity(0)
             yaw_motor.setVelocity(0)
             if (index != -1):
@@ -182,13 +226,13 @@ def main():
                 changed = False
                 
         if (changed):
-            print(f"w - {w} ; fr - {Mecanum_Calculator(Vx,Vy,w,0)} ; fl - {Mecanum_Calculator(Vx,Vy,w,1)} ; \
-                bl - {Mecanum_Calculator(Vx,Vy,w,2)} ; br - {Mecanum_Calculator(Vx,Vy,w,3)}")
+            print(f"w:{w} ; fr:{Mecanum_Calculator(Vx,Vy,w,0,wheelsMaxSpeeds[0])} ; fl:{Mecanum_Calculator(Vx,Vy,w,1,wheelsMaxSpeeds[1])}")
+            print(f"bl:{Mecanum_Calculator(Vx,Vy,w,2,wheelsMaxSpeeds[2])} ; br:{Mecanum_Calculator(Vx,Vy,w,3,wheelsMaxSpeeds[3])}")
         
-        wheels[0].setVelocity(Mecanum_Calculator(Vx,Vy,w,0))
-        wheels[1].setVelocity(Mecanum_Calculator(Vx,Vy,w,1))
-        wheels[2].setVelocity(Mecanum_Calculator(Vx,Vy,w,2))
-        wheels[3].setVelocity(Mecanum_Calculator(Vx,Vy,w,3))
+        wheels[0].setVelocity(Mecanum_Calculator(Vx,Vy,w,0,wheelsMaxSpeeds[0]))
+        wheels[1].setVelocity(Mecanum_Calculator(Vx,Vy,w,1,wheelsMaxSpeeds[1]))
+        wheels[2].setVelocity(Mecanum_Calculator(Vx,Vy,w,2,wheelsMaxSpeeds[2]))
+        wheels[3].setVelocity(Mecanum_Calculator(Vx,Vy,w,3,wheelsMaxSpeeds[3]))
         
         
 if __name__ == '__main__':
